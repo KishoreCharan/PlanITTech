a7e59a8d0592f2461765263da7634968c3a89a89
KishoreCharan <rscharan65@gmail.com>
2025-02-26 17:33:38 +1000

    cypress tests added

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('PlanITTechnical Asset ment Testsg', () => {
+  it('Test Case 1: Verify error messages', () => {
+    cy.visit('https://jupiter.cloud.planittesting.com/')
+    cy.contains('Contact');
+    cy.get("ul li a").contains("Contact").click();
+    cy.get("div a.btn-contact").click();
+    
+
+
+    
   })
 })
